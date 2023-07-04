3fa30d5874842421df5237f68bdee7b0f07ba0bd
RinFiz <121906192+RinFiz@users.noreply.github.com>
2023-07-04 12:20:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,7 @@
-function addItem() {
-  const itemInput = document.getElementById('itemInput');
-  if (itemInput.value.trim() === '') return;
-
-  const li = document.createElement('li');
-  const removeBtn = document.createElement('span');
-
-  const id = Date.now();
-  li.id = `item-${id}`;
-
-  const itemText = document.createElement('span');
-  itemText.textContent = itemInput.value;
-  itemText.classList.add('item-text');
-
-  removeBtn.innerHTML = '×';
-  removeBtn.classList.add('remove');
-
-  removeBtn.addEventListener('click', () => {
-    removeItem(li.id);
-  });
-
-  li.appendChild(itemText);
-  li.appendChild(removeBtn);
-  itemList.appendChild(li);
-
-  itemInput.value = '';
-  updateListNumbers();
-}
-
-function removeItem(itemId) {
-  const item = document.getElementById(itemId);
-  item.parentNode.removeChild(item);
-  updateListNumbers();
-}
-
 function updateListNumbers() {
   const items = itemList.children;
   for (let i = 0; i < items.length; i++) {
     const itemText = items[i].querySelector('.item-text');
-    const itemNumber = `${i + 1}. `;
-    itemText.textContent = itemText.textContent.replace(/^\d+\.\s*/, itemNumber);
+    itemText.textContent = `${i + 1}. ${itemText.textContent.substring(itemText.textContent.indexOf('.') + 2)}`;
   }
 }
