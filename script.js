65c7c3b47801c988d3bdc9bdd569032c1d261764
RinFiz <121906192+RinFiz@users.noreply.github.com>
2023-07-04 12:22:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -37,6 +37,6 @@ function updateListNumbers() {
   const items = itemList.children;
   for (let i = 0; i < items.length; i++) {
     const itemText = items[i].querySelector('.item-text');
-    itemText.textContent = `${i + 1}. ${itemText.textContent.substring(itemText.textContent.indexOf('.') + 2)}`;
+    itemText.textContent = `${i + 1}. ${itemText.textContent.substring(itemText.textContent.indexOf(' ') + 1)}`;
   }
 }
