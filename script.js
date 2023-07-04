b8433a4adffe33447dfc4960ae849f96ccb70a91
RinFiz <121906192+RinFiz@users.noreply.github.com>
2023-07-04 12:21:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+function addItem() {
+  const itemInput = document.getElementById('itemInput');
+  if (itemInput.value.trim() === '') return;
+
+  const li = document.createElement('li');
+  const removeBtn = document.createElement('span');
+
+  const id = Date.now();
+  li.id = `item-${id}`;
+
+  const itemText = document.createElement('span');
+  itemText.textContent = itemInput.value;
+  itemText.classList.add('item-text');
+
+  removeBtn.innerHTML = '×';
+  removeBtn.classList.add('remove');
+
+  removeBtn.addEventListener('click', () => {
+    removeItem(li.id);
+  });
+
+  li.appendChild(itemText);
+  li.appendChild(removeBtn);
+  itemList.appendChild(li);
+
+  itemInput.value = '';
+  updateListNumbers();
+}
+
+function removeItem(itemId) {
+  const item = document.getElementById(itemId);
+  item.parentNode.removeChild(item);
+  updateListNumbers();
+}
+
 function updateListNumbers() {
   const items = itemList.children;
   for (let i = 0; i < items.length; i++) {
